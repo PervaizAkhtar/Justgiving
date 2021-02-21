b24e0341468aebba7b2aa04357905952b47b491a
PervaizAkhtar <m.akhtar101@gmail.com>
2021-02-21 14:14:45 +0000

    initial

---
+++ JG.FinTechTest/JG.FinTechTest.API/Controllers/GiftAidController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JG.FinTechTest.API.Controllers
+{
+    [ApiController]
+    [Route("api/giftaid")]
+    public class GiftAidController : ControllerBase
+    {
+      
+
+        private readonly ILogger<GiftAidController> _logger;
+
+        public GiftAidController(ILogger<GiftAidController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello Word");
+        }
+    }
+}
